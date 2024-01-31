3c64e9273cb14aed299f0b3b89bd13a3b3472bcb
kit <kitherman@gmail.com>
2024-01-31 08:34:50 -0500

    updates

--- flake.nix
+++ flake.nix
@@ -9,40 +9,46 @@
       url = "github:neovim/neovim/stable?dir=contrib";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
+  outputs = { self, nixpkgs, neovim,flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
 
-  outputs = { self, nixpkgs, neovim }:
-    let
-      overlayFlakeInputs = prev: final: {
-        neovim = neovim.packages.x86_64-linux.neovim;
-      };
-
-      overlayNeovim = prev: final: {
-        customNeovim = import ./packages/nvimConfig.nix {
-          pkgs = final;
+      let
+        overlayFlakeInputs = prev: final: {
+          neovim = neovim.packages.x86_64-linux.neovim;
         };
-      };
 
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        overlays = [ overlayFlakeInputs overlayNeovim ];
-      };
+        overlayNeovim = prev: final: {
+          customNeovim = import ./packages/nvimConfig.nix {
+            pkgs = final;
+          };
+        };
 
-    in {
-      packages.x86_64-linux.default = pkgs.customNeovim;
-      apps.x86_64-linux.default = {
-        type = "app";
-        program = "${pkgs.customNeovim}/bin/nvim";
-      };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ overlayFlakeInputs overlayNeovim ];
+        };
 
-      devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            customNeovim
-            texlab
-            texliveBasic
-          ];
-      };
+      in {
 
-    };
+        packages.${system}.default = pkgs.customNeovim;
+
+        apps.${system}.default = {
+          type = "app";
+          program = "${pkgs.customNeovim}/bin/nvim";
+        };
+
+        devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              customNeovim
+              texlab
+              texliveBasic
+            ];
+        };
+      }
+
+    );
 }
 
